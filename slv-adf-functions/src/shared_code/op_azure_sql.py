06096f4cb2b3cd522b541d0581e0af0377ef9750
Sotirios Alpanis <SAlpanis@slv.vic.gov.au>
2023-03-14 17:09:47 +1100

    Re-staged after pre-commit hooks

--- slv-adf-functions/src/shared_code/op_azure_sql.py
+++ slv-adf-functions/src/shared_code/op_azure_sql.py
@@ -1,11 +1,11 @@
-import datetime
 import logging
 import os
+from datetime import datetime
 
 from . import op_libcal, shared_azure, shared_constants
 
 
-def upload_libcal_data_to_azure():
+def upload_libcal_data_to_azure(last_date_retrieved):
     """Coordinates the retrieval of data from Libcal and subsequent upload to Azure SQL database
 
     Returns:
@@ -18,15 +18,10 @@ def upload_libcal_data_to_azure():
 
     if not table_exists:
         shared_azure.create_azure_sql_table(environment, prefix)
-        last_date_retrieved = datetime.strptime(
-            shared_constants.EARLIEST_DATE, "%Y-%m-%d"
-        ).date()
-    else:
-        last_date_retrieved = shared_azure.get_most_recent_date_in_db(
-            environment, prefix="stg"
-        )
 
     logging.info(f"Last date retrieved: {last_date_retrieved}")
+
+    last_date_retrieved = datetime.strptime(last_date_retrieved, "%Y-%m-%d").date()
 
     libcal_data_for_upload = op_libcal.get_booking_data_to_upload(
         environment, last_date_retrieved
